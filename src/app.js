ff041cf3626662c2ffc96973bb4940333f24a268
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-19 07:20:10 +0530

    ES5 Module added & imported to our app.js file

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-const greeting = 'Hello World';
-console.log(greeting);
+/*
+  - It's Common.js, not ES2015 Code
+  - We dont' have to use .js in the end after calling our module './mymodule1.js' , that's how bring modules that we install with npm (node_modules)
+  - Example :- for adding express.js we need to require -> require('express'); That's the actual node module
+*/
+const person = require('./mymodule1');
+
+console.log(person.name); 
---
+++ src/mymodule1.js
@@ -0,0 +1,6 @@
+// common.js Module  - whatever I will put here, availabe to other files. Onlyway to access is by adding module.exports
+
+module.exports = {
+  name: 'Jigyasu',
+  email: 'test@test.com'
+}
