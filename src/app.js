ff5c0d22b962da9b29a61145683bb93621c8a053
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-19 11:12:55 +0530

    Test Drive of our Fake REST API using GET

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 import { http } from './http';
+
+// Get Post on DOM Load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+   http.get('http://localhost:3000/posts')
+   // It's asyn function, so it'll return a promise
+   .then(data => console.log(data))
+   .catch(err => console.log(err));
+}
