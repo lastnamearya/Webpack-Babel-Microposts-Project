b9c7da6540819043967ae0258a451dde247bf3a4
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-19 12:04:57 +0530

    New Post added to our fake REST API i.e db.json

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
 import { http } from './http';
 import { ui } from './ui';
 
-// Get Post on DOM Load
+// Get Posts on DOM Load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add Post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
    http.get('http://localhost:3000/posts')
    // It's asyn function, so it'll return a promise
    .then(data => ui.showPosts(data))
    .catch(err => console.log(err));
+}
+
+// Submit Post
+function submitPost(){
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create POST Request
+  http.post('http://localhost:3000/posts', data)
+  .then(data => {
+    getPosts();
+  })
+  .catch(err => console.log(err));
 }
